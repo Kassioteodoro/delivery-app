9c70c9a7a5c4501b2716a7c0c395da357e2f4a39
lucdainez <lucdainez@gmail.com>
2023-02-28 16:10:38 -0300

    feat: pagina inicial de login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Login from './pages/login';
+import Login from './pages/login-test';
 
 function App() {
   document.title = 'App delivery';
