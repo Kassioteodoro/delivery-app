99fd1aff28357eca5e2cc344dfd378901213ac43
Douglas Alves <douglas_dca@hotmail.com>
2023-02-28 18:01:17 -0300

    feat: user registration api

--- back-end/src/services/login.service.js
+++ back-end/src/services/login.service.js
@@ -3,6 +3,7 @@ const { createToken } = require('../auth/jwtFunctions');
 
 const login = async (email, password) => {
   const user = await User.findOne({ where: { email, password } });
+  console.log(user);
   if (!user) return { type: 'NOT FOUND', message: 'Not found' };
   const { password: _password, ...userWithoutPassword } = user.dataValues;
   const token = createToken(userWithoutPassword);
